d1cb6f7e1a32ffc3dd22ed15ff27c91432518996
heyolaniran <olaniran.abd@gmail.com>
2024-03-18 21:54:49 +0100

    fix: redis params

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,7 +2,5 @@ import { Redis } from '@upstash/redis'
 
 const redis = new Redis({
   url: 'https://us1-smart-basilisk-40872.upstash.io',
-  token: 'AZ-oASQgYjQwZjQ3ZDgtMzg2YS00ZTRlLTg2OTMtNTQ3ZmMyNjFlNDVlNjc2Mjk4ZjQ5YzE0NDVmZjljMzQ5OTM3OTNmZTZjYjk=',
+  token: process.env.REDIS_TOKEN!,
 })
-
-const data = await redis.set('foo', 'bar');
