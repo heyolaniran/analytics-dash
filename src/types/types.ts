17408b816b3464ffaca03319a53aa969b7d1717f
heyolaniran <olaniran.abd@gmail.com>
2024-03-19 17:40:40 +0100

    feat : types for analytics components

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,15 @@
+import { analytics } from "@/utils/analytics"
+
 export type AnalyticsArgs  = {
     retention?: number
 }
 
 export type TrackOption = {
     persist? : boolean
+}
+
+export interface AnalyticsDashboadProps {
+    avgVisitorsPerDay : string 
+    amountVisitorsToday : number
+    timeSeriesPagesViews : Awaited<ReturnType<typeof analytics.retrieveDays>>
 }
